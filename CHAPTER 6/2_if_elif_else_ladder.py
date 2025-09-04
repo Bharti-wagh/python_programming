623b9f1c73fbcb61f0efe5e30f299e68a0d5d8db
Bharti krushna wagh <bharatiwagh148@gmail.com>
2025-09-04 17:02:23 +0530

    Add files via upload

---
+++ CHAPTER 6/2_if_elif_else_ladder.py
@@ -0,0 +1,13 @@
+###################################################### if elif else ladder #######################################################
+age=int(input("Enter age :"))
+if (age>18):
+    print("your are above 18")
+
+elif (age<0):
+    print("you are entering an invalid age")
+
+elif (age==0):
+    print("you are entering an invalid age")
+
+else:
+    print("you are below 18 ")
---
+++ CHAPTER 6/3_multiple_if_statement.py
@@ -0,0 +1,20 @@
+
+age=int(input("Enter age :"))
+#if statement number 1
+if (age%2==0):
+    print("Even")
+#End of if statement 1
+
+#if statement number 2
+if (age>18):
+    print("your are above 18")
+
+elif (age<0):
+    print("you are entering an invalid age")
+
+elif (age==0):
+    print("you are entering an invalid age")
+
+else:
+    print("you are below 18 ")
+#End of if statement 2
